a343789573b520087df03bd25cb52797adc4d57f
eybaek <eybaek22@gmail.com>
2023-11-09 00:03:47 +0900

    Update 0001-two-sum.js

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,16 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let answer = [];
     for (let i=0; i<nums.length; i++) {
-        for (let j=i+1; j<nums.length; j++) {
-            // if (nums[i] + nums[j] === target) {
-            //     answer.push(i);
-            //     answer.push(j);
-            // }
-            if (nums[j] == target - nums[i]) {
-                return Array.from([i, j]);
-            }
+      for (let j=i+1; j<nums.length; j++) {
+        if (nums[j] == target - nums[i]) {
+          return Array.from([i, j])
         }
+      }
     }
 };

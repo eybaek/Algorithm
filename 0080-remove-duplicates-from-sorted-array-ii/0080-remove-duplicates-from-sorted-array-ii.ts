443e78b5ad53aece5f2d290c21222166d34f60c7
eybaek <eybaek22@gmail.com>
2023-12-14 14:58:13 +0900

    Time: 63 ms (63.07%), Space: 45 MB (57.58%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.ts
@@ -0,0 +1,15 @@
+function removeDuplicates(nums: number[]): number {
+ if (nums.length < 2) {
+   return nums.length;
+ }
+  
+  let k = 2;
+  
+  for (let i=2; i<nums.length; i++) {
+    if (nums[i] !== nums[k-2] || nums[i] !== nums[k-1]) {
+      nums[k] = nums[i]
+      k++
+    }
+  }
+  return k
+};

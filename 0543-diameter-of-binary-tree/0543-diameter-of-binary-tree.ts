fe1fc82940fd3e69c10e26435095de41f30f9416
eybaek <eybaek22@gmail.com>
2024-02-28 07:01:25 +0900

    Time: 68 ms (53.06%), Space: 55 MB (6.46%) - LeetHub

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function diameterOfBinaryTree(root: TreeNode | null): number {
+    let diameter = 0;
+    dfs(root);
+    return diameter;
+    
+    function dfs(node) {
+        if (!node) return 0;
+        
+        const left = dfs(node.left);
+        const right = dfs(node.right);
+        
+        diameter = Math.max(diameter, left+right);
+        
+        return 1 + Math.max(left, right);
+    }
+    
+};
